87939ff718ed8c624a456f0e21d80d2bc70cec09
ermias1068 <ermiasbeyene@gmail.com>
2023-08-14 22:57:18 -0700

    update on the source code in order to switch value of a and b

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-def swap(a, b):
-    a = a + b
-    b = a - b
-    a = a - b
-    return a, b
-
-a = 10
-b = 89
-
-a, b = swap(a, b)
-
-print(f"a={a} - b={b}")
-
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
