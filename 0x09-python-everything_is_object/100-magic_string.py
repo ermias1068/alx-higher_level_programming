2ffbfcb0b33ccfbe4fd57ca69ff2e9cd917d0b9a
ermias1068 <ermiasbeyene@gmail.com>
2023-09-06 06:37:07 -0700

    update in 100-magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l += ["Holberton"]
-    return ", ".join(l)
+def magic_string(List_of_str=[]):
+    List_of_str += ["BestSchool"]
+    return (", ".join(List_of_str))
